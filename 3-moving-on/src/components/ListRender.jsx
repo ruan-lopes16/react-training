3c96d32c2c9b087c01b1c842d51b73d0f084e809
ruan-lopes16 <sruan507@gmail.com>
2026-01-24 16:15:30 -0300

    previous state

--- 3-moving-on/src/components/ListRender.jsx
+++ 3-moving-on/src/components/ListRender.jsx
@@ -10,13 +10,26 @@ const ListRender = () => {
         {id: 3, name: "Clara", age: 24}
     ])
 
+    // 6 - previous state
+    const deleteRandom = () => {
+        // achar um numero aleatorio com base na quantidade de itens na lista
+        const randomNumber = Math.floor(Math.random() * 4)
+
+        setUsers((prevUsers) =>  
+            // quero que faça um filtro nos usuários => retorna todos os usuarios com o id diferente no numero aleatorio
+            prevUsers.filter((user) => randomNumber !== user.id))
+    }
+
   return (
     <div>
         {/* render sem key*/}
         <ul>
+            {/*
             {list.map((item) => ( // loop para exibi elementos
                 <li>{item}</li>
             ))}
+            */}
+
             {/*gambiarra para parar o warning no console - não aconselhavel*/}
             {list.map((item, i) => ( // loop para exibi elementos
                 <li key={i}>{item}</li>
@@ -32,6 +45,9 @@ const ListRender = () => {
                 </li>
             ))}
         </ul>
+        <hr />
+        {/* 6- previous state */}
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }
