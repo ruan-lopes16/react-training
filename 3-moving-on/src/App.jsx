3ed18f6b08a75d660ecec669b913f270fa714cdb
ruan-lopes16 <sruan507@gmail.com>
2026-01-26 22:07:12 -0300

    reuse of props with loop

--- 3-moving-on/src/App.jsx
+++ 3-moving-on/src/App.jsx
@@ -18,6 +18,13 @@ import ShowUserName from './components/ShowUserName'
 // 9 - desestruturando props
 import CarDetails from './components/CarDetails'
 
+// 11 - renderização de listas com componente
+const cars = [
+  { id: 1, brand: "Ferrari", model: "SF90 XX", color: "Amarelo", km: 0 },
+  { id: 2, brand: "KIA", model:"Cerato", color: "Branco", km: 120040 },
+  { id: 3, brand: "Renault", model:"Clio RS", color: "Azul", km: 32000 },
+]
+
 function App() {
 
   return (
@@ -46,6 +53,11 @@ function App() {
       {/* reutilizando props */}
       <CarDetails brand="Chevrolet" model="Corsa" km={98001} color="silver" />
       <CarDetails brand="Fiat" model="Palio" km={125058} color="silver" />
+
+      {/*renderização de listas com componente*/}
+      {cars.map((car) => (
+        <CarDetails key={car.id} brand={car.brand} model={car.model} km={car.km} color={car.color} />
+      ))}
     </div>
   )
 }
